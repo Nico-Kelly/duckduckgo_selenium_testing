3b48c94abe0deba863e57026c0e06135a95b5a22
Jorge <103543373+AustralCoder@users.noreply.github.com>
2026-03-10 17:50:16 -0300

    First Commit, this will be a practice suite for Selenium Web Driver

---
+++ tests/test_search.py
@@ -0,0 +1,19 @@
+def test_basic_duckduckgo_search():
+
+#Given the DuckDuckGo home page is displayed
+#TODO
+
+# When the user searches for "David Bowie"
+#TODO
+
+# Then the search result title contains "David Bowie"
+#TODO
+
+# and the search query is "David Bowie"
+#TODO
+
+#and the search result links pertain to "David Bowie"
+#TODO
+
+    
+    raise Exception("Incomplete test")
