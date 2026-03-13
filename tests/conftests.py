8efeb713ba6206d47b454d9d85f556e5ec161f0c
Jorge <103543373+AustralCoder@users.noreply.github.com>
2026-03-12 23:45:37 -0300

    feat: implementing the pages directory, __init__.py and the search.py for Page Object Models

--- tests/conftests.py
+++ tests/conftests.py
@@ -5,12 +5,17 @@ import selenium.webdriver
 @pytest.fixture
 def browser():
 
+    #initialize the ChromeDriver instance
     b = selenium.webdriver.Chrome()
 
+
+    #Make its calls wait up to 10 seconds for elements to appear
     b.implicitly_wait(10)
 
+    #Return the WebDriver instance for the setup
     yield b
 
+    #Quit the WebDriver instance for the cleanup
     b.quit()
 
 
